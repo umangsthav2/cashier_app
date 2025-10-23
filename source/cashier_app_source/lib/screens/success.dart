9c5e6585463a5453119c37cf8084c707645b9df4
unknown <mangaleeshrestha@gmail.com>
2025-10-23 22:44:33 +0545

    Created Success Page

---
+++ source/cashier_app_source/lib/screens/success.dart
@@ -0,0 +1,76 @@
+import 'package:cashier_app_source/screens/home.dart';
+import 'package:cashier_app_source/screens/otp.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../utils/colors.dart';
+
+class SuccessScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext bc) {
+    return Scaffold(
+      backgroundColor: Color(brandBlack),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(bc).size.height,
+          child: Padding(
+            padding: EdgeInsets.all(30),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom:100),
+                    child: SvgPicture.asset("assets/icons/success.svg",height:150),
+                  ),
+                  Text(
+                    "Success!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      color: Color(primaryColor),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:25),
+                    child: Text(
+                      "You have successfully Withdrawn in pubg game and can start Playing it",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Color(brandWhite),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(primaryColor),
+                        minimumSize: Size(double.infinity, 50),
+                      ),
+                      onPressed: () {
+                        Navigator.push(bc,MaterialPageRoute(builder: (newpage)=>HomeScreen()));
+                      },
+                      child: Text(
+                        "Done",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Color(brandWhite),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
